52ddf9b6c977a488f2ff0943406dcfedf8699096
Dimitrije Stanković <diki.zajecar@gmail.com>
2024-05-16 20:50:31 +0200

    Create App.xaml.cs

---
+++ OOT_PZ_Kursevi/OOT_PZ_Kursevi/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OOT_PZ_Kursevi
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
