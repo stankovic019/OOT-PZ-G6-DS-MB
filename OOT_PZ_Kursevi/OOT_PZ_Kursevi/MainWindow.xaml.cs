1f3a7dbd37f343a93d63042b3d22e7b587d64453
Dimitrije Stanković <diki.zajecar@gmail.com>
2024-05-18 05:13:02 +0200

    tab1
    napravljen izgled taba 1, ostaje jos funkcionalnost.
    
    izmenjeno:
    .xaml
    .xaml.cs

--- OOT_PZ_Kursevi/OOT_PZ_Kursevi/MainWindow.xaml.cs
+++ OOT_PZ_Kursevi/OOT_PZ_Kursevi/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace OOT_PZ_Kursevi
 {
@@ -16,9 +18,35 @@ namespace OOT_PZ_Kursevi
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Kurs> dostupniKursevi = new ObservableCollection<Kurs> ();
+        private ObservableCollection<Kurs> nedostupniKursevi = new ObservableCollection<Kurs> ();
+
         public MainWindow()
         {
             InitializeComponent();
+
+           
+            dostupniKursevi.Add(new Kurs(1, "C Programiranje", "niga", 32, "", "Predavanja"));
+            dostupniKursevi.Add(new Kurs(2, "OOP C#", "niiga", 44, "", "Vezbe"));
+            dostupniKursevi.Add(new Kurs(3, "Vega IT", "opalac", 0, "", "Praksa"));
+            
+            dostupniKurseviDGV.ItemsSource = dostupniKursevi;
+
+            
+            
+            nedostupniKursevi.Add(new Kurs(3, "Vega IT", "opalac", 0, "", "Praksa"));
+            nedostupniKursevi.Add(new Kurs(2, "OOP C#", "niiga", 44, "", "Vezbe"));
+            nedostupniKursevi.Add(new Kurs(1, "C Programiranje", "niga", 32, "", "Predavanja"));
+
+            nedostupniKurseviDGV.ItemsSource = nedostupniKursevi;
+
+
+
+        }
+
+        private void dostupniKurseviDGV_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+
         }
     }
 }
