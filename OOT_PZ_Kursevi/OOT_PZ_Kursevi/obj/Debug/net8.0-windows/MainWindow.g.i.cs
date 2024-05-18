ae27f74c47a5f182a70f2e307067285fa15e2b8f
Dimitrije Stanković <diki.zajecar@gmail.com>
2024-05-18 06:44:31 +0200

    dodate datoteke

--- OOT_PZ_Kursevi/OOT_PZ_Kursevi/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ OOT_PZ_Kursevi/OOT_PZ_Kursevi/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace OOT_PZ_Kursevi {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/OOT_PZ_Kursevi;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/OOT_PZ_Kursevi;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
