d86fd6f127053d1b13b3f3daad07dbd76663f899
MajaBogicevic <maja.bogicevic01@gmail.com>
2024-05-22 00:56:14 +0200

    estetika

--- OOT_PZ_Kursevi/OOT_PZ_Kursevi/obj/Debug/net8.0-windows/Dodaj.g.i.cs
+++ OOT_PZ_Kursevi/OOT_PZ_Kursevi/obj/Debug/net8.0-windows/Dodaj.g.i.cs
@@ -111,13 +111,13 @@ namespace OOT_PZ_Kursevi {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.4.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/OOT_PZ_Kursevi;V1.0.0.0;component/dodaj.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/OOT_PZ_Kursevi;component/dodaj.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Dodaj.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -127,7 +127,7 @@ namespace OOT_PZ_Kursevi {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.4.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
